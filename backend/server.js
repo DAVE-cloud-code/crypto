5248b9a3f1446a1dd92ca366b2ef8e62d54ba5b9
DAVE-cloud-code <daviduche161@gmail.com>
2025-04-30 19:58:42 +0200

    cors issues

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'https://oreantrade.onrender.com',
-  'https://oreantrade..com'
+  'https://oreantrade..com',
+  'https://www.oreantrade.com'
 ];
 
 app.use(cors({
